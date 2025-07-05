a24427d9f715cee3db12af132bf03d9a1ff01811
Kari Meghana <karimeghana02@gmail.com>
2025-07-05 16:05:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 //your code here
-document.addEventListener("DOMContentLoaded", function() {
-  const button = document.getElementById("myButton");
-  if (button) {
-    button.addEventListener("click", function() {
-      alert("Button clicked!");
-    });
-  }
-});
